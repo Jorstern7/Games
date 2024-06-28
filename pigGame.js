627ed79b10cca2226509aae53be3cd0043ff0efc
Jorstern7 <jorsternalcide@gmail.com>
2024-06-27 21:22:17 -0400

    Score Limit

--- pigGame.js
+++ pigGame.js
@@ -72,7 +72,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       // If so, finnish the game
       playing = false;
       diceEl.classList.add("hidden");
